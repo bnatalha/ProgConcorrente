a7ba1727d6af2b7762ecfc94b66bd9f933d59046
bnatalha <natalia_eletro@hotmail.com>
2021-12-17 21:45:57 -0300

    add parse arguments

--- 01matriz/multiplym.py
+++ 01matriz/multiplym.py
@@ -1,33 +1,19 @@
 from threading import Thread, Lock
 from time import sleep
+import sys
 
-# TODO: usar mapa ou lista?
+def printMatrix(M):
+    for i in M.keys():
+        print(M[i])
 
-""" 
-    1, 2
-    3, 4
-"""
-matrix_A = {
-    0: {0: 1,
-        1: 2},
-    1: {0: 3,
-        1: 4},
-}
-
-matrix_B = {
-    0: {0: -1,
-        1: 3},
-    1: {0: 4,
-        1: 2},
-}
 
 def readMatrix(filename):
     matrix = {}
-    with open("in/"+ filename + ".txt", 'r') as f:
+    with open(filename, 'r') as f:
         dims =[int(num) for num in f.readline().rstrip('\n').strip().split(' ')]
         # tratar exceções caso não tenha isto
         if (len(dims) != 2):
-            raise Exception("The first line must have exactly 2 numbers (the dimensions of the matrix")
+            raise Exception("The first line must have exactly 2 numbers (the dimensions of the matrix)")
         
         for i in range(0, dims[0]):
             line = f.readline().strip().rstrip('\n').split(' ')
@@ -57,9 +43,9 @@ def multiplySeq(A,B,n):
     return C
 
 def multiplyCon(A,B,n):
-    C = {}  # result
     threads = []
 
+    C = {}  # result
     for i in range(0,n):
         C[i] = {} # prefill matrix dictionaries
         for j in range(0,n):
@@ -72,7 +58,60 @@ def multiplyCon(A,B,n):
 
     return C
 
-#print(multiplySeq(matrix_A,matrix_B,2))
-print(multiplySeq(readMatrix("A4x4"), readMatrix("B4x4"), 4))               
-# print(readMatrix("B4x4"))
+def runAlg(_dimension, _type):
+    
+    inputFilename = lambda prefix, n : f'in/{prefix}{n}x{n}.txt'
+    
+    # build filenames
+    matrix_a_path = inputFilename("A", str(_dimension))
+    matrix_b_path = inputFilename("B", str(_dimension))
 
+    # read matrixes
+    matrix_a = readMatrix(matrix_a_path)
+    matrix_b = readMatrix(matrix_b_path)
+
+    # multiply
+    result = {}
+    if _type == "S":
+        result = multiplySeq(matrix_a, matrix_b, _dimension)
+    elif _type == "C":
+        result = multiplyCon(matrix_a, matrix_b, _dimension)
+    elif _type == "X":  # compare two results
+        s = multiplySeq(matrix_a, matrix_b, _dimension)
+        c = multiplyCon(matrix_a, matrix_b, _dimension)
+        for i in range(0, _dimension):
+            for j in range(0, _dimension):
+                if s[i][j] != c[i][j] :
+                    print(f's[{i}][{j}] = {s[i][j]}; c = {c[i][j]}')
+    else:
+        raise Exception(f'There is no implementation for _type={_type}')
+
+    return result
+
+
+## Program arguments
+args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+if __name__ == "__main__":
+
+    isPowerOfTwo = lambda x : (x & (x-1)) == 0
+
+    # args parser
+    if len(args) != 2:
+        print("wrong program usage")
+    else: 
+        dimensions = int(args[0])
+        alg_type = args[1]
+        if dimensions < 4 or 2048 < dimensions or (not isPowerOfTwo(dimensions)):
+            print('matrix dimensions n needs to be a power of 2, with 4 < n <= 2048')
+            print(f'recieved: {dimension}')
+        elif alg_type not in ['S', 'C', 'X']:
+            print(f'enter the algorithm that wil run ({C} for the concurrent and {S} for the sequential version)')
+            print(f'recieved: {alg_type}')
+        else:
+            result = runAlg(dimensions, alg_type)
+            print(result)
+    
+    # print(multiplySeq(matrix_A,matrix_B,2))
+    # print(multiplySeq(readMatrix("A4x4"), readMatrix("B4x4"), 4))               
+    # print(readMatrix("B4x4"))
