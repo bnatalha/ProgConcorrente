cef5f387336303afebae59b02a109b298026e420
bnatalha <natalia_eletro@hotmail.com>
2021-12-17 22:54:31 -0300

    refact

--- 01matriz/multiplym.py
+++ 01matriz/multiplym.py
@@ -1,39 +1,24 @@
 from threading import Thread, Lock
 from time import sleep
-import sys
-
-def printMatrix(M):
-    for i in M.keys():
-        print(M[i])
-
-
-def readMatrix(filename):
-    matrix = {}
-    with open(filename, 'r') as f:
-        dims =[int(num) for num in f.readline().rstrip('\n').strip().split(' ')]
-        # tratar exceções caso não tenha isto
-        if (len(dims) != 2):
-            raise Exception("The first line must have exactly 2 numbers (the dimensions of the matrix)")
-        
-        for i in range(0, dims[0]):
-            line = f.readline().strip().rstrip('\n').split(' ')
-            # print(line) #DEBUG
-            for j in range(0, len(line)): # ou dims[-1]
-                val = int(line[j].strip())
-                if i not in matrix:
-                   matrix[i] = {j: val}
-                if i in matrix:
-                   matrix[i].update({j: val})
-    return matrix
-    # print(matrix) #DEBUG
+from aux import *
 
 def calcCell(A,B,C,i,j,n):
+    """ Calculates the value of the of the cell C[i][j] where
+        C = A x B and attributes it to C.
+
+        All matrixes have nxn dimensions.   
+    """
     soma = 0
     for k in range(0,n):
         soma = soma + A[i][k] * B[k][j]
     C[i][j] = soma
 
 def multiplySeq(A,B,n):
+    """ Using only one thread, returns the value of the matrix C where
+        C = A x B
+
+        All matrixes have nxn dimensions.   
+    """
     C = {}
     for i in range(0,n):
         C[i] = {} # prefill matrix dictionaries
@@ -43,34 +28,42 @@ def multiplySeq(A,B,n):
     return C
 
 def multiplyCon(A,B,n):
+    """ Using concurrent proggraming, returns the value of the matrix C where
+        C = A x B
+
+        All matrixes have nxn dimensions.   
+    """
     threads = []
 
     C = {}  # result
     for i in range(0,n):
         C[i] = {} # prefill matrix dictionaries
         for j in range(0,n):
+            # start threads
             t = Thread(target=calcCell, args=(A, B, C, i,j,n))
             threads.append(t)
             t.start()
             
+    # wait for threads to finish
     for t in threads:
         t.join()
 
     return C
 
 def runAlg(_dimension, _type):
-    
-    inputFilename = lambda prefix, n : f'in/{prefix}{n}x{n}.txt'
-    
-    # build filenames
+    """ Will execute the matrix multiplication as defined by
+        the _dimension and the _type  
+    """
+
+    # build the filenames
     matrix_a_path = inputFilename("A", str(_dimension))
     matrix_b_path = inputFilename("B", str(_dimension))
 
-    # read matrixes
+    # read the matrixes
     matrix_a = readMatrix(matrix_a_path)
     matrix_b = readMatrix(matrix_b_path)
 
-    # multiply
+    # multiply using one of the strategies
     result = {}
     if _type == "S":
         result = multiplySeq(matrix_a, matrix_b, _dimension)
@@ -89,29 +82,13 @@ def runAlg(_dimension, _type):
     return result
 
 
-## Program arguments
-args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
 if __name__ == "__main__":
 
-    isPowerOfTwo = lambda x : (x & (x-1)) == 0
-
-    # args parser
-    if len(args) != 2:
-        print("wrong program usage")
-    else: 
-        dimensions = int(args[0])
-        alg_type = args[1]
-        if dimensions < 4 or 2048 < dimensions or (not isPowerOfTwo(dimensions)):
-            print('matrix dimensions n needs to be a power of 2, with 4 < n <= 2048')
-            print(f'recieved: {dimension}')
-        elif alg_type not in ['S', 'C', 'X']:
-            print(f'enter the algorithm that wil run ({C} for the concurrent and {S} for the sequential version)')
-            print(f'recieved: {alg_type}')
-        else:
-            result = runAlg(dimensions, alg_type)
+    try :
+        params = parseArgs()
+        if len(params) == 2:
+            result = runAlg(params[0], params[1])
             print(result)
-    
-    # print(multiplySeq(matrix_A,matrix_B,2))
-    # print(multiplySeq(readMatrix("A4x4"), readMatrix("B4x4"), 4))               
-    # print(readMatrix("B4x4"))
+    except Exception as err:
+        print(err)
