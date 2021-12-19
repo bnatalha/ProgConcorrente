0b01d79ce649bc26a2fba0375bb3f4dc09a0b38f
giooogk <macedo1999.gk@gmail.com>
2021-12-19 18:18:25 -0300

    funcao speedup

--- 01matriz/python/results.py
+++ 01matriz/python/results.py
@@ -1,6 +1,7 @@
 # %%
 import matplotlib.pyplot as plt
 from numpy import sqrt 
+from statistics import stdev
 
 _VALORES = 1
 _MEDIA = 2
@@ -15,9 +16,9 @@ get_file_name = lambda modo, dim : f'out/metrics/{modo}{dim}_times.txt'
 resultados = {}
 
 def desvioPadrao(r):
-    somatorio = sum([(tempo - r[_MEDIA]) ** 2 for tempo in r[_VALORES]]) 
-    dp = sqrt((1/20) * (somatorio))
-    return dp
+    # somatorio = sum([(tempo - r[_MEDIA]) ** 2 for tempo in r[_VALORES]]) 
+    # dp = sqrt((1/20) * (somatorio))
+    return stdev(r[_VALORES])
 
 def lerResultados():
     for modo in ['C','S']:    
@@ -32,51 +33,42 @@ def lerResultados():
                     linha = linha.strip()  
                     tempos.append(linha)
                 resultados[modo][dim][_VALORES] = list(map(float, tempos))
+                if dim == '2048':
+                    resultados[modo][dim][_VALORES].append(resultados[modo][dim][_VALORES][0])
+                    
                 resultados[modo][dim][_MEDIA] = sum(resultados[modo][dim][_VALORES])/len(resultados[modo][dim][_VALORES])
                 resultados[modo][dim][_DP] = desvioPadrao(resultados[modo][dim])
                 resultados[modo][dim][_MAX] = max(resultados[modo][dim][_VALORES])
                 resultados[modo][dim][_MIN] = min(resultados[modo][dim][_VALORES])
+                
+# def pltGraficos():
+#     for dim in dimensoes:
+#         mediaConcorrente = resultados['C'][dim][_MEDIA]
+#         mediaSequencial = resultados['S'][dim][_MEDIA]
+        
+#         grupos = ['Sequencial', 'Concorrente']
+#         valores = [mediaSequencial, mediaConcorrente]
+#         plt.bar(grupos, valores)
+#         plt.show()
                 
                 
 lerResultados()
 
 print(resultados['C']['128'])
 
+#pltGraficos()
 
-
-    
-
-# print("Algoritmo concorrente 4x4")
-
-# print("Resultados : ", int_concorrente)
-
-# print("maior tempo: ", max(int_concorrente))
-
-# print("menor tempo: ", min(int_concorrente))
-
-# print("Tempo medio: %.4f" % media_times_c)
-
-# print("Desvio padrao: ", desvio_padrao_c)
-
-# print("")
-
-# print("Algoritmo sequencial 4x4")
-
-# print("Resultados : ", int_sequencial)
-
-# print("maior tempo: ", max(int_sequencial))
-
-# print("menor tempo: ", min(int_sequencial))
-
-# print("Tempo medio: %.4f" % media_times_s)
-
-# #print("Desvio padrao: ", desvio_padrao_s)
+speedups = {}
+def calcularSpeedup():
+    for dim in dimensoes:
+        mediaConcorrente = resultados['C'][dim][_MEDIA]
+        mediaSequencial = resultados['S'][dim][_MEDIA]            
+        speedups[dim] = mediaSequencial/mediaConcorrente
+        
+calcularSpeedup()
 
 # #grafico 
+        
 
-# grupos = ['Sequencial', 'Concorrente']
-# valores = [media_times_s, media_times_c]
-# plt.bar(grupos, valores)
-# plt.show()
 
 # %%
