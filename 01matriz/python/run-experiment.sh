d89fa6241a012fe4140355e67ef56f55480fd767
bnatalha <natalia_eletro@hotmail.com>
2021-12-19 19:19:28 -0300

    minor tweaks on scripts

--- 01matriz/python/run-experiment.sh
+++ 01matriz/python/run-experiment.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-PROGRAM_NAME='multiplym.py'
+PROGRAM_NAME='multiplym'
 MAX_REPS=20
 
 exec_program(){
     dimension=$1
     exec_mode=$2
-    python3 $PROGRAM_NAME $dimension $exec_mode
+    ./$PROGRAM_NAME $dimension $exec_mode
 }
 
 for mode in 'S' 'C'
 do 
     for dimension in 4 8 16 32 64 128 256 1024 2048
-    # for dimension in 8
     do
         output="out/metrics/${mode}${dimension}_times.txt"
         echo $(date) >> $output
-        for reps in {1..2}
+        for reps in {1..20}
         do
             timing=$(exec_program $dimension $mode)
             echo "[$reps/$MAX_REPS] $mode $dimension $timing"
