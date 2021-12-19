10c4fd277159618b8d7040d02199f62f9ae0efa5
giooogk <macedo1999.gk@gmail.com>
2021-12-19 17:18:19 -0300

    wip calculos do experimento

--- 01matriz/python/results.py
+++ 01matriz/python/results.py
@@ -2,81 +2,81 @@
 import matplotlib.pyplot as plt
 from numpy import sqrt 
 
+_VALORES = 1
+_MEDIA = 2
+_DP = 3
+_MIN = 4
+_MAX = 5
+
 dimensoes = ["4", "8", "16", "32", "64", "128", "256", "1024", "2048"]
 
-arquivo = open('metrics/C4_times.txt', "r")
-concorrente = []
-for linha in arquivo:
-    linha = linha.strip()  
-    concorrente.append((linha))
-concorrente.pop(0)
-arquivo.close()
+get_file_name = lambda modo, dim : f'out/metrics/{modo}{dim}_times.txt'
+
+resultados = {}
+
+def desvioPadrao(r):
+    somatorio = sum([(tempo - r[_MEDIA]) ** 2 for tempo in r[_VALORES]]) 
+    dp = sqrt((1/20) * (somatorio))
+    return dp
+
+def lerResultados():
+    for modo in ['C','S']:    
+        resultados[modo]={}
+        for dim in dimensoes:
+            nomeArquivo = get_file_name(modo,dim)
+            resultados[modo][dim] = {}
+            with open(nomeArquivo, "r") as arquivo:
+                tempos = []
+                arquivo.readline()
+                for linha in arquivo:
+                    linha = linha.strip()  
+                    tempos.append(linha)
+                resultados[modo][dim][_VALORES] = list(map(float, tempos))
+                resultados[modo][dim][_MEDIA] = sum(resultados[modo][dim][_VALORES])/len(resultados[modo][dim][_VALORES])
+                resultados[modo][dim][_DP] = desvioPadrao(resultados[modo][dim])
+                resultados[modo][dim][_MAX] = max(resultados[modo][dim][_VALORES])
+                resultados[modo][dim][_MIN] = min(resultados[modo][dim][_VALORES])
+                
+                
+lerResultados()
+
+print(resultados['C']['128'])
 
 
-arquivo = open('metrics/S4_times.txt', "r")
-sequencial = []
-for linha in arquivo:
-    linha = linha.strip()  
-    sequencial.append((linha))
-sequencial.pop(0)
-arquivo.close()
-
-
-int_concorrente = list(map(float, concorrente))
-int_sequencial = list(map(float, sequencial))
-
-media_times_c = sum(int_concorrente)/len(int_concorrente)
-media_times_s = sum(int_sequencial)/len(int_sequencial)
-
-somatorio = 0.0
-
-for tempo in int_concorrente:
-    variante = (tempo - media_times_c) ** 2
-    #print("variante: ", variante)
-    
-    somatorio += variante
-    
-desvio_padrao_c = sqrt((1/20) * (somatorio))
-
-#for tempo in int_concorrente:
-    #variante = (tempo - media_times_s) ** 2
-    #somatorio += variante
-
-#desvio_padrao_s = sqrt((1/20) * (somatorio))
 
     
 
-print("Algoritmo concorrente 4x4")
+# print("Algoritmo concorrente 4x4")
 
-print("Resultados : ", int_concorrente)
+# print("Resultados : ", int_concorrente)
 
-print("maior tempo: ", max(int_concorrente))
+# print("maior tempo: ", max(int_concorrente))
 
-print("menor tempo: ", min(int_concorrente))
+# print("menor tempo: ", min(int_concorrente))
 
-print("Tempo medio: %.4f" % media_times_c)
+# print("Tempo medio: %.4f" % media_times_c)
 
-print("Desvio padrao: ", desvio_padrao_c)
+# print("Desvio padrao: ", desvio_padrao_c)
 
-print("")
+# print("")
 
-print("Algoritmo sequencial 4x4")
+# print("Algoritmo sequencial 4x4")
 
-print("Resultados : ", int_sequencial)
+# print("Resultados : ", int_sequencial)
 
-print("maior tempo: ", max(int_sequencial))
+# print("maior tempo: ", max(int_sequencial))
 
-print("menor tempo: ", min(int_sequencial))
+# print("menor tempo: ", min(int_sequencial))
 
-print("Tempo medio: %.4f" % media_times_s)
+# print("Tempo medio: %.4f" % media_times_s)
 
-#print("Desvio padrao: ", desvio_padrao_s)
+# #print("Desvio padrao: ", desvio_padrao_s)
 
-#grafico 
+# #grafico 
 
-grupos = ['Sequencial', 'Concorrente']
-valores = [media_times_s, media_times_c]
-plt.bar(grupos, valores)
-plt.show()
+# grupos = ['Sequencial', 'Concorrente']
+# valores = [media_times_s, media_times_c]
+# plt.bar(grupos, valores)
+# plt.show()
 
 # %%
