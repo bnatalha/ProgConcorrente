00b5cce7251b554d59b36adf4927d692588d511d
bnatalha <natalia_eletro@hotmail.com>
2022-02-06 14:21:37 -0300

    mais de uma equipe

--- 03go/main.go
+++ 03go/main.go
@@ -3,90 +3,102 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"time"
 )
 
-// estrutura para guardar as informações de um corredor
-type corredor struct {
-	duracao time.Duration
-	id      int
-	time    int
+// estrutura para guardar as informações de um Corredor
+type Corredor struct {
+	tempo       time.Duration
+	tempo_total time.Duration
+	id          int
+	time        int
 }
 
-const MAX_CORREDORES = 4
+type PorTempo []Corredor
 
-func run(id_corredor, id_time int, raia chan corredor, chegada chan int) {
-	// tempo percorrido
+func (a PorTempo) Len() int           { return len(a) }
+func (a PorTempo) Less(i, j int) bool { return a[i].tempo_total < a[j].tempo_total }
+func (a PorTempo) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+const MAX_CORREDORES = 4 // total de corredores por time
+const MAX_EQUIPES = 4    // total de times
+
+func correr(id_corredor, id_equipe int, raia, chegada chan Corredor) {
+	// espera o bastão chegar para começar a correr
+	ultimo_corredor := <-raia
+
+	// calcular tempo que será gasto na corrida
 	seed := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(seed)
-	duracao := time.Second * time.Duration(random.Intn(5))
-	// duracao := time.Millisecond * time.Duration(random.Intn(100))
+	tempo := time.Millisecond * time.Duration(random.Intn(100))
 
-	time.Sleep(duracao)
-	fmt.Print("Corredor ", id_corredor, " do time ", id_time, " correu por ", duracao)
+	// corre
+	time.Sleep(tempo)
 
-	raia <- corredor{duracao: duracao, id: id_corredor, time: id_time}
+	tempo_total := tempo + ultimo_corredor.tempo_total
+	corredor_atual := Corredor{tempo: tempo, id: id_corredor, time: id_equipe, tempo_total: tempo_total}
+	fmt.Println("Corredor", id_corredor, "da equipe", id_equipe, "correu por", tempo, "\t TEMPO:", tempo_total)
 
 	// fecha a pista para a equipe após o último corredor terminar de correr
+	// ou manda o próximo corredor
 	if MAX_CORREDORES == id_corredor {
 		close(raia)
-		chegada <- id_time // notifica o canal da linha de chegada
+		chegada <- corredor_atual // notifica o canal da linha de chegada
 		return
 	} else {
-		go run(id_corredor+1, id_time, raia, chegada) // lança o próximo corredor
+		go correr(id_corredor+1, id_equipe, raia, chegada) // posiciona o próximo corredor
+		raia <- corredor_atual                             // passa o bastão
 	}
+}
+
+// simular a largada, deixando o bastão pronto para o primeiro
+// corredor possa pegá-lo e começar a correr
+func prepararBastao(raia chan Corredor) {
+	raia <- Corredor{tempo_total: 0, tempo: 0}
 }
 
 func main() {
 
-	// cada canal receberá as informações de uma equipe
-	raia1 := make(chan corredor)
-	raia2 := make(chan corredor)
-	chegada := make(chan int) // canal responsável por notificar se todas as esquipes cruzaram a chegada
+	// cada canal será uma raia para os corredores de uma equipe
+	var raias [MAX_EQUIPES]chan Corredor
+	for i := 0; i < MAX_EQUIPES; i++ {
+		raias[i] = make(chan Corredor)
+	}
 
-	go func() {
-		run(1, 1, raia1, chegada)
-	}()
-	go func() {
-		run(1, 2, raia2, chegada)
-	}()
+	// canal responsável por notificar a chegada das equipes
+	chegada := make(chan Corredor)
 
-	tempo1 := time.Duration(0)
-	tempo2 := time.Duration(0)
-	menorTempo := time.Duration(0)
-	remainingTeams := 2
+	// preparando os batões para que as equipes	possam largar
+	for i := 0; i < MAX_EQUIPES; i++ {
+		go prepararBastao(raias[i])
+	}
+
+	// ====================== LARGADA ==========================
+
+	for i := 0; i < MAX_EQUIPES; i++ {
+		go correr(1, i, raias[i], chegada)
+	}
+
+	var stats_finais []Corredor
+	remainingTeams := MAX_EQUIPES
 	for remainingTeams > 0 {
-		select {
-		case c, ok := <-raia1:
-			if ok {
-				tempo1 = tempo1 + c.duracao
-				fmt.Println("\tTEMPO TOTAL:", tempo1)
-			}
-		case c, ok := <-raia2:
-			if ok {
-				tempo2 = tempo2 + c.duracao
-				fmt.Println("\tTEMPO TOTAL:", tempo2)
-			}
-		case t := <-chegada:
-			tempo := time.Duration(0)
-			if t == 1 {
-				tempo = tempo1
-			} else {
-				tempo = tempo2
-			}
-			fmt.Println("Tempo final do time", t, tempo)
-			remainingTeams = remainingTeams - 1
-		}
-
+		t := <-chegada
+		stats_finais = append(stats_finais, t)
+		fmt.Println("Equipe", t.time, "\t==================CHEGADA==================\t", t.tempo_total)
+		remainingTeams = remainingTeams - 1
 	}
 
-	// calculando equipe que correu mais rápido
-	menorTempo = tempo1
-	vencedor := 1
-	if tempo1 > tempo2 {
-		menorTempo = tempo2
-		vencedor = 2
-	}
+	// ====================== CHEGADA ==========================
 
-	fmt.Println("\nEquipe", vencedor, "venceu com", menorTempo)
+	// ordena tempos e equipes
+	sort.Sort(PorTempo(stats_finais))
+
+	// imprime os resultados finais
+	fmt.Print("\nEquipe ", stats_finais[0].time, ": ", stats_finais[0].tempo_total, "\tEQUIPE VENCEDORA")
+	for i := 1; i < MAX_EQUIPES; i++ {
+		fmt.Print("\nEquipe ", stats_finais[i].time, ": ", stats_finais[i].tempo_total)
+	}
+	fmt.Println()
+
 }
